f0615ee94468f2124b0f5e06c82ce42de96afc35
Hosein <h.naseri1990@gmail.com>
2017-12-03 18:53:22 +0330

    add body parser to project and modify post routes

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,12 @@ router.get('/ninjas', function(req,res){
 
 // add a new ninjas to the db
 router.post('/ninjas', function(req,res){
-
+    console.log(req.body)
+    res.send({
+        type: 'POST',
+        name: req.body.name,
+        rank: req.body.rank
+    })
 });
 
 // update a new ninjas to the db
