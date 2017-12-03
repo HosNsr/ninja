0428cae53be32423c96605f5db083152d1db01c0
Hosein Naseri <h.naseri1990@gmail.com>
2017-12-03 16:19:44 +0330

    first commit

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// get a list a ninjss from db
+router.get('/ninjas', function(req,res){
+
+});
+
+// add a new ninjas to the db
+router.post('/ninjas', function(req,res){
+
+});
+
+// update a new ninjas to the db
+router.put('/ninjas/:id', function(req,res){
+    
+});
+
+// delete a new ninjas to the db
+router.delete('/ninjas/:id', function(req,res){
+    
+});
+
+module.exports = router;
