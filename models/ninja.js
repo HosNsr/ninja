cc4535968dc09c08bb86c14f5ff5f56682a9df01
Hosein <h.naseri1990@gmail.com>
2017-12-03 23:02:49 +0330

    modify put middlware and  create a data in db

--- models/ninja.js
+++ models/ninja.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create ninja schema & model
+// create ninja Schema & model
 const NinjaSchema = new Schema({
-    name : {
-        type : String,
-        required : [true, 'Name field is required']
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
     },
-    rank : {
-        type : String,
+    rank: {
+        type: String
     },
-    available : {
-        type : Boolean,
-        default : false
-    },
+    available: {
+        type: Boolean,
+        default: false
+    }
     // add in geo location
-})
+});
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
 
--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,27 @@
-const express = require('express');
+const express = require ('express');
 const router = express.Router();
+const Ninja = require('../models/ninja');
 
-// get a list a ninjss from db
-router.get('/ninjas', function(req,res){
-
+// get a list of ninjas from the db
+router.get('/ninjas', function(req, res){
+    res.send({type: 'GET'});
 });
 
-// add a new ninjas to the db
-router.post('/ninjas', function(req,res){
-    console.log(req.body)
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        rank: req.body.rank
-    })
+// add a new ninja to the db
+router.post('/ninjas', function(req, res){
+    Ninja.create(req.body).then(function(ninja){
+        res.send(ninja);
+    });
 });
 
-// update a new ninjas to the db
-router.put('/ninjas/:id', function(req,res){
-    
+// update a ninja in the db
+router.put('/ninjas/:id', function(req, res){
+    res.send({type: 'PUT'});
 });
 
-// delete a new ninjas to the db
-router.delete('/ninjas/:id', function(req,res){
-    
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res){
+    res.send({type: 'DELETE'});
 });
 
 module.exports = router;
